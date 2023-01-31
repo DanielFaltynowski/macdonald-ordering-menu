6809db2567ded0fce930e34caa62aac8e79b259d
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2023-01-31 18:48:25 +0100

    pattern searching

--- frontend/src/components/OrderType.js
+++ frontend/src/components/OrderType.js
@@ -7,19 +7,13 @@ import axios from "axios";
 const OrderType = ({takeout, setTakeout}) => {
     const data = useSelector((state) => state.currentOrder.data)
     const dispatch = useDispatch()
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/products')
-            .then(res => {
-                dispatch(setCurrentData(res.data))
-            })
-    }, []);
     const handleTakeoutChoice = () => {
         setTakeout(!takeout)
     }
     return (
         <div className="flex flex-col items-center justify-center">
-            {data === null && <div className="w-3/4 flex justify-evenly m-10"><div>Loading...</div></div>}
-            {data !== null && <div className="w-3/4 flex justify-evenly m-10">
+            {/*{data === null && <div className="w-3/4 flex justify-evenly m-10"><div>Loading...</div></div>}*/}
+            <div className="w-3/4 flex justify-evenly m-10">
                 <Link to="/getstarted/menu">
                     <div
                         className="w-96 h-96 border border-8 border-solid rounded-3xl flex justify-center items-center hover:bg-gray-50">
@@ -32,7 +26,7 @@ const OrderType = ({takeout, setTakeout}) => {
                         <div className="text-gray-500 font-black text-6xl">Takeout</div>
                     </div>
                 </Link>
-            </div>}
+            </div>
             {/*{(!isPending) && error !== null && <div className="w-3/4 flex justify-evenly m-10"><div>{error}</div></div>}*/}
         </div>
     )
--- frontend/src/components/OrderingMenu.js
+++ frontend/src/components/OrderingMenu.js
@@ -1,27 +1,45 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ProductsList from "./ProductsList";
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {setBar} from "../redux/currentOrder";
+import {setBar, setCurrentData} from "../redux/currentOrder";
+import axios from "axios";
+import {useFormik} from "formik";
 
 const OrderingMenu = () => {
     const order = useSelector((state) => state.currentOrder.order)
     const dispatch = useDispatch();
     const data = useSelector((state) => state.currentOrder.data)
-    const [burgers, setBurgers] = useState(data.products.filter(prod => prod.type === "burger"))
-    const [sandwiches, setSandwiches] = useState(data.products.filter(prod => prod.type === "sandwich"));
-    const [nuggets, setNuggets] = useState(data.products.filter(prod => prod.type === "nuggets"));
-    const [fands, setFands] = useState(data.products.filter(prod => prod.type === "fands"));
-    const [beverages, setBeverages] = useState(data.products.filter(prod => prod.type === "beverage"))
-    const [coffees, setCoffees] = useState(data.products.filter(prod => prod.type === "coffee"));
-    const condition = burgers === null || sandwiches === null || nuggets === null || fands === null || beverages === null || coffees === null
+    const [burgers, setBurgers] = useState([])
+    const [sandwiches, setSandwiches] = useState([]);
+    const [nuggets, setNuggets] = useState([]);
+    const [fands, setFands] = useState([]);
+    const [beverages, setBeverages] = useState([])
+    const [coffees, setCoffees] = useState([]);
+    const [condition, setCondition] = useState(true)
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/products')
+            .then(res => {
+                dispatch(setCurrentData(res.data))
+                return res.data
+            })
+            .then((gotData) => {
+                setBurgers(gotData.products.filter(prod => prod.type === "burger"))
+                setSandwiches(gotData.products.filter(prod => prod.type === "sandwich"))
+                setNuggets(gotData.products.filter(prod => prod.type === "nuggets"))
+                setFands(gotData.products.filter(prod => prod.type === "fands"))
+                setBeverages(gotData.products.filter(prod => prod.type === "beverage"))
+                setCoffees(gotData.products.filter(prod => prod.type === "coffee"))
+            })
+            .then(() => {
+                setCondition(false)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, []);
     const template = (
         <div className="flex flex-col gap-10 pt-24">
-            <div>
-                <Link to="/getstarted/payfor" onClick={() => dispatch(setBar(false))}>
-                    <button className="bg-blue-700 text-white font-black text-5xl p-5 rounded-2xl disabled:bg-gray-400 disabled:text-gray-600 hover:bg-blue-300 hover:text-blue-900" disabled={order.length === 0}>Accept & Pay</button>
-                </Link>
-            </div>
             <ProductsList groupOf={burgers} header="BURGERS"></ProductsList>
             <ProductsList groupOf={sandwiches} header="SANDWICHES"></ProductsList>
             <ProductsList groupOf={nuggets} header="NUGGETS"></ProductsList>
@@ -30,9 +48,74 @@ const OrderingMenu = () => {
             <ProductsList groupOf={coffees} header="COFFEES"></ProductsList>
         </div>
     )
+    const formik = useFormik({
+        initialValues: {
+            pattern: ''
+        },
+        onSubmit: values => {
+            if (values.pattern === '') {
+                axios.get('http://127.0.0.1:5000/products')
+                    .then(res => {
+                        dispatch(setCurrentData(res.data))
+                        return res.data
+                    })
+                    .then((gotData) => {
+                        setBurgers(gotData.products.filter(prod => prod.type === "burger"))
+                        setSandwiches(gotData.products.filter(prod => prod.type === "sandwich"))
+                        setNuggets(gotData.products.filter(prod => prod.type === "nuggets"))
+                        setFands(gotData.products.filter(prod => prod.type === "fands"))
+                        setBeverages(gotData.products.filter(prod => prod.type === "beverage"))
+                        setCoffees(gotData.products.filter(prod => prod.type === "coffee"))
+                    })
+                    .then(() => {
+                        setCondition(false)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            } else {
+                axios.get(`http://127.0.0.1:5000/products/${values.pattern}`)
+                    .then(res => {
+                        dispatch(setCurrentData(res.data))
+                        return res.data
+                    })
+                    .then(gotData => {
+                        setBurgers(gotData.products.filter(prod => prod.type === "burger"))
+                        setSandwiches(gotData.products.filter(prod => prod.type === "sandwich"))
+                        setNuggets(gotData.products.filter(prod => prod.type === "nuggets"))
+                        setFands(gotData.products.filter(prod => prod.type === "fands"))
+                        setBeverages(gotData.products.filter(prod => prod.type === "beverage"))
+                        setCoffees(gotData.products.filter(prod => prod.type === "coffee"))
+                    })
+                    .then(() => {
+                        setCondition(false)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            }
+        }
+    })
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="w-3/4">
+                <div className="mt-16">
+                    <div className="flex gap-5 items-center">
+                        <Link to="/getstarted/payfor" onClick={() => dispatch(setBar(false))}>
+                            <button className="bg-blue-700 text-white font-black text-5xl p-5 rounded-2xl disabled:bg-gray-400 disabled:text-gray-600 hover:bg-blue-300 hover:text-blue-900" disabled={order.length === 0}>Accept & Pay</button>
+                        </Link>
+                        <form onSubmit={formik.handleSubmit}>
+                            <label className="font-black ml-16">Search:</label>
+                            <input type="text" className="border border-blue-500 rounded-lg ml-5 mr-5 p-2"
+                                   id="pattern"
+                                   name="pattern"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.pattern}/>
+                            <button className="bg-green-700 text-white font-black text-3xl p-3 rounded-2xl hover:bg-green-300 hover:text-green-900">Find</button>
+                        </form>
+                    </div>
+                </div>
 
                 {condition && (
                     <div className="flex flex-col gap-10 pt-24">
